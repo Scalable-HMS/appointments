b7b2728c0fe76085e2e060abe87d49c28dd669c5
Shubham Gupta <shubham.gupta2956@gmail.com>
2022-03-24 03:29:44 +0530

    Integrate sentry
    Signed-off-by: Shubham Gupta <shubham.gupta2956@gmail.com>

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/wryonik/appointment
 go 1.17
 
 require (
-	github.com/gin-gonic/gin v1.7.4
+	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/gorm v1.9.16
 )
 
 require (
+	github.com/getsentry/sentry-go v0.13.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
 	"github.com/wryonik/appointment/controllers"
 	"github.com/wryonik/appointment/models"
 
@@ -8,6 +12,17 @@ import (
 )
 
 func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://9b4a3071bed048bcb43bfafffbaab6e7@o1176298.ingest.sentry.io/6273811",
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+
 	r := gin.Default()
 
 	// Connect to database
