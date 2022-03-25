0ef0ab38db778356c681908b7e4f19cf82ca370d
Scar26 <mmatty26@gmail.com>
2022-03-25 05:47:48 +0530

    update config
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- src/models/setup.go
+++ src/models/setup.go
@@ -1,7 +1,9 @@
 package models
 
-
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -9,11 +11,13 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "scar:passloll@tcp(10.46.144.2:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-  	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
+	} else {
+		fmt.Println("Database connection established")
 	}
 
 	database.AutoMigrate(&Appointment{})
